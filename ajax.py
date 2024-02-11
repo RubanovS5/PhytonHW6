5de5be89284e0479e5a59623e0a1418bca4716f7
Александр Рубанов <uttka90@gmail.com>
2024-02-11 14:31:39 +0300

    Update ajax.py

--- ajax.py
+++ ajax.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
+
 Chrome_driver = webdriver.Chrome()
 waiter = WebDriverWait(Chrome_driver, 40)
 
@@ -13,7 +13,10 @@ button_locator = (By.ID, "ajaxButton")
 blue_button = waiter.until(EC.element_to_be_clickable(button_locator))
 blue_button.click()
 
-green_locator = (By.XPATH, "//div[@class='bg-success']")
+green_locator = (By.XPATH, "//p[@class = 'bg-success']")
+
 green_text = waiter.until(EC.visibility_of_element_located(green_locator)).text
 
 print(green_text)
+
+Chrome_driver.quit()
